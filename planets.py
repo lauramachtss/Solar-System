d58128c0c3f73e7c99f27c441774034fc636891f
Laura <166519168+lauramachts0@users.noreply.github.com>
2024-08-10 14:24:11 -0300

    Add files via upload

--- planets.py
+++ planets.py
@@ -1,17 +1,20 @@
+import random
+
 planets = [
-    {"name": "Mercury", "size": 20, "color": "gray", "distance": "50px", "duration": "10s"},
-    {"name": "Venus", "size": 25, "color": "orange", "distance": "80px", "duration": "18s"},
-    {"name": "Earth", "size": 30, "color": "blue", "distance": "110px", "duration": "30s"},
-    {"name": "Mars", "size": 22, "color": "red", "distance": "150px", "duration": "60s"},
-    {"name": "Jupiter", "size": 50, "color": "brown", "distance": "220px", "duration": "150s"},
-    {"name": "Saturn", "size": 45, "color": "yellow", "distance": "300px", "duration": "300s"},
-    {"name": "Uranus", "size": 35, "color": "lightblue", "distance": "400px", "duration": "500s"},
-    {"name": "Neptune", "size": 34, "color": "darkblue", "distance": "500px", "duration": "600s"},
+    {"name": "Mercury", "size": 20, "color": "gray", "distance": "80px", "duration": "10s"},
+    {"name": "Venus", "size": 25, "color": "orange", "distance": "150px", "duration": "18s"},
+    {"name": "Earth", "size": 30, "color": "blue", "distance": "230px", "duration": "30s"},
+    {"name": "Mars", "size": 22, "color": "red", "distance": "320px", "duration": "60s"},
+    {"name": "Jupiter", "size": 50, "color": "brown", "distance": "450px", "duration": "150s"},
+    {"name": "Saturn", "size": 45, "color": "yellow", "distance": "580px", "duration": "300s"},
+    {"name": "Uranus", "size": 35, "color": "lightblue", "distance": "700px", "duration": "500s"},
+    {"name": "Neptune", "size": 34, "color": "darkblue", "distance": "820px", "duration": "600s"},
 ]
 
-# Gerando o HTML dos planetas
+# Gerando o HTML dos planetas com rotação inicial aleatória
 planet_html = ""
 for planet in planets:
+    initial_rotation = random.randint(0, 360)
     planet_html += f'''
     <div class="planet" style="
         width: {planet['size']}px;
@@ -19,6 +22,7 @@ for planet in planets:
         background-color: {planet['color']};
         --distance: {planet['distance']};
         animation-duration: {planet['duration']};
+        transform: rotate({initial_rotation}deg) translateX(var(--distance));
     ">
         <span class="planet-name">{planet['name']}</span>
     </div>
