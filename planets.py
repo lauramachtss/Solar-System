4bee9062d174556825d2f93c6d3a538076bb4124
Laura <166519168+lauramachts0@users.noreply.github.com>
2024-08-10 17:06:11 -0300

    Add files via upload

--- planets.py
+++ planets.py
@@ -6,26 +6,45 @@ planets = [
     {"name": "Earth", "size": 30, "color": "blue", "distance": "230px", "duration": "30s"},
     {"name": "Mars", "size": 22, "color": "red", "distance": "320px", "duration": "60s"},
     {"name": "Jupiter", "size": 50, "color": "brown", "distance": "450px", "duration": "150s"},
-    {"name": "Saturn", "size": 45, "color": "yellow", "distance": "580px", "duration": "300s"},
+    {"name": "Saturn", "size": 45, "color": "yellow", "distance": "580px", "duration": "300s", "has_rings": True},
     {"name": "Uranus", "size": 35, "color": "lightblue", "distance": "700px", "duration": "500s"},
     {"name": "Neptune", "size": 34, "color": "darkblue", "distance": "820px", "duration": "600s"},
 ]
 
+# Gerando o HTML dos planetas com rotação inicial aleatória e anéis para Saturno
 planet_html = ""
 for planet in planets:
     initial_rotation = random.randint(0, 360)
-    planet_html += f'''
-    <div class="planet" style="
-        width: {planet['size']}px;
-        height: {planet['size']}px;
-        background-color: {planet['color']};
-        --distance: {planet['distance']};
-        animation-duration: {planet['duration']};
-        transform: rotate({initial_rotation}deg) translateX(var(--distance));
-    ">
-        <span class="planet-name">{planet['name']}</span>
-    </div>
-    '''
+    if planet.get("has_rings"):
+        planet_html += f'''
+        <div class="planet" style="
+            width: {planet['size']}px;
+            height: {planet['size']}px;
+            background-color: {planet['color']};
+            --distance: {planet['distance']};
+            animation-duration: {planet['duration']};
+            transform: rotate({initial_rotation}deg) translateX(var(--distance));
+        ">
+            <div class="rings"></div>
+            <span class="planet-name">{planet['name']}</span>
+        </div>
+        '''
+    else:
+        planet_html += f'''
+        <div class="planet" style="
+            width: {planet['size']}px;
+            height: {planet['size']}px;
+            background-color: {planet['color']};
+            --distance: {planet['distance']};
+            animation-duration: {planet['duration']};
+            transform: rotate({initial_rotation}deg) translateX(var(--distance));
+        ">
+            <span class="planet-name">{planet['name']}</span>
+        </div>
+        '''
 
+# Salvando o código HTML gerado
 with open("planets.html", "w") as file:
     file.write(planet_html)
+
+print("HTML dos planetas gerado com sucesso!")
